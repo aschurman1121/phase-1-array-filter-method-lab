5118678224964b7a37d4274ae1c215629be2e6f2
Alexandra Schurman <alexandraschurman@gmail.com>
2022-07-30 17:57:43 -0400

    commit tree 5fadb7b8f1d0f1595ec793ff8b4b72136168bbdc

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+
+
+
+function findMatching (arr, name) {
+    return arr.filter(possibleMatch => 
+        possibleMatch.toLowerCase() === name.toLowerCase()
+    )
+
+}
+
+
+
+// function findMatching(source, sought) {
+//     return source.filter( possibleMatch =>
+//       possibleMatch.toLowerCase() === sought.toLowerCase()
+//     )
+//   }
+
+
+function fuzzyMatch (arr, nameToMatch) {
+    return arr.filter(possibleMatch => possibleMatch.toLowerCase().indexOf(nameToMatch.toLowerCase()) === 0 )
+        }
